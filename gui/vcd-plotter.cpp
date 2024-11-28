f54698dcaebd1e9e6e8b22be5c60494c990324bb
Xenona <xenona11x@gmail.com>
2024-11-28 23:19:07 +0300

    Added vector drawing

--- gui/vcd-plotter.cpp
+++ gui/vcd-plotter.cpp
@@ -26,58 +26,44 @@
 // todo
 // add enter to add selected dumps
 
-VCDPlotter::VCDPlotter(VCDData *data, QWidget *parent) : QWidget(parent) {
+VCDPlotter::VCDPlotter(VCDData *data, QWidget *parent) : QWidget(parent) 
 
+{
   this->data = data;
-
-  QVBoxLayout *verticalLayout_12 = new QVBoxLayout(this);
-
-  QSplitter *splitter = new QSplitter(this);
+  QVBoxLayout* verticalLayout_12 = new QVBoxLayout(this);
+  QSplitter * splitter = new QSplitter(this);
   splitter->setOrientation(Qt::Horizontal);
   splitter->setChildrenCollapsible(false);
-  QWidget *verticalLayoutWidget_4 = new QWidget(splitter);
-  QHBoxLayout *horizontalLayout = new QHBoxLayout(verticalLayoutWidget_4);
+  QWidget* verticalLayoutWidget_4 = new QWidget(splitter);
+  QHBoxLayout* horizontalLayout = new QHBoxLayout(verticalLayoutWidget_4);
   horizontalLayout->setSpacing(0);
   horizontalLayout->setContentsMargins(0, 0, 0, 0);
-
   this->selected_dumps = new QTreeWidget(verticalLayoutWidget_4);
-  this->vertScroll = this->selected_dumps->verticalScrollBar();
-
   this->selected_dumps->header()->setVisible(false);
   this->selected_dumps->setHorizontalScrollBarPolicy(Qt::ScrollBarAlwaysOn);
-  this->selected_dumps->setSelectionMode(QAbstractItemView::MultiSelection);
   // this->selected_dumps_list->setMaximumWidth(600);
-  this->selected_dumps->setMinimumWidth(100);
+  // this->selected_dumps_list->setMinimumWidth(50);
   this->selected_dumps->setGeometry({0, 0, 1000, 1000});
   this->selected_dumps->show();
-
-  // horizontalLayout->addWidget(selected_dumps);
-
-  // this->vertScroll = new QScrollBar(verticalLayoutWidget_4);
-  // this->vertScroll->setOrientation(Qt::Vertical);
-
-  // horizontalLayout->addWidget(this->vertScroll);
-
+  this->selected_dumps->setSelectionMode(QAbstractItemView::MultiSelection);
+  horizontalLayout->addWidget(selected_dumps);
+  this->vertScroll = this->selected_dumps->verticalScrollBar();
   splitter->addWidget(verticalLayoutWidget_4);
-  QWidget *verticalLayoutWidget = new QWidget(splitter);
-  QVBoxLayout *verticalLayout_2 = new QVBoxLayout(verticalLayoutWidget);
+  QWidget * verticalLayoutWidget = new QWidget(splitter);
+  QVBoxLayout * verticalLayout_2 = new QVBoxLayout(verticalLayoutWidget);
   verticalLayout_2->setSpacing(0);
   verticalLayout_2->setContentsMargins(0, 0, 0, 0);
+
   this->view = new VCDGraphicsView(this, this->varsList, this->dumpsList, this->data,
                                    verticalLayoutWidget);
-
+  
   verticalLayout_2->addWidget(this->view);
-
   this->horizScroll = new QScrollBar(verticalLayoutWidget);
   this->horizScroll->setOrientation(Qt::Horizontal);
-
   verticalLayout_2->addWidget(this->horizScroll);
-
   splitter->addWidget(verticalLayoutWidget);
-
   verticalLayout_12->addWidget(splitter);
-  splitter->setSizes({10});
-
+  // this->leftFOVborder = this->data->timepoints[0];
   QObject::connect(this->vertScroll, &QScrollBar::actionTriggered, this,
                    [this](int value) {
                      // todo fix the magic number
--- gui/waves.cpp
+++ gui/waves.cpp
@@ -3,9 +3,13 @@
 #include <QPainterPath>
 #include <climits>
 #include <cmath>
+#include <qfontmetrics.h>
 #include <qgraphicsitem.h>
 #include <qlogging.h>
+#include <qnamespace.h>
 #include <qpainterpath.h>
+#include <qpoint.h>
+#include <string>
 
 // todo
 // get all destructors into work
@@ -21,30 +25,42 @@ void Waves::paint(QPainter *painter, const QStyleOptionGraphicsItem *option,
   this->top->setUpdatesEnabled(false);
 
   const int WAVES_GAP = 5;
+  int HEXAGONS_STEP;
 
-  int y = WAVES_GAP;
+  float y = WAVES_GAP;
   int a = this->top->top->leftFOVborder;
-  int b = this->top->top->rightFOVborder;
+  int b = this->top->top->rightFOVborder - 100;
+  if (this->top->data->timepoints.size())
+    HEXAGONS_STEP = ((this->top->data->timepoints.back().time -
+                      this->top->data->timepoints[0].time) /
+                     (b - a)) %
+                    10;
+  else {
+    HEXAGONS_STEP = 1;
+  }
   int width = this->top->size().width();
-  int lineHeight = this->top->itemRect.height();
+  float lineHeight = this->top->itemRect.height();
 
   // iterating over variables starting from visible ones (scrollHeight idx)
   for (int i = this->top->scrollHeight; i < this->top->vars.length(); i++) {
 
     QPainterPath path;
+    QPainterPath auxPath;
     path.moveTo(0, y);
+
+    float prevScenePos = 0;
 
     float prevFloat = 0;
     long long prev = 0;
     bool inited = false;
 
+    bool isVector = false;
+
     float yLow = y + lineHeight - WAVES_GAP;
-    float yHalf = yLow/2;
+    float yHalf = y + (yLow - y) / 2;
 
     // iterating over time points (events)
     for (auto &timepoint : this->top->data->timepoints) {
-
-      // if j is existing time event
 
       DumpData dump = timepoint.data;
       int t = timepoint.time;
@@ -52,53 +68,122 @@ void Waves::paint(QPainter *painter, const QStyleOptionGraphicsItem *option,
       float scenePos = ((t - a) / (float)(b - a)) * width;
       if (scenePos >= 0 && scenePos <= width) {
 
-        int idx = this->isScalar(dump, this->top->vars[i].identifier);
+        int idx = this->isVector(dump, this->top->vars[i].identifier);
+        qDebug() << idx;
         if (idx != -1) {
-          if (!inited) {
-            prev = dump.scals[idx].value;
-            inited = true;
-            // start low if the first bit is 0
-            if (prev == 0) {
-              path.moveTo(0, yLow);
-            }
-          } else {
-
-            // draw an angle depending on state
-            if (prev == 0 && dump.scals[idx].value == 1) {
-              path.lineTo(scenePos, yLow);
-              path.lineTo(scenePos, y);
-            }
-            if (prev == 1 && dump.scals[idx].value == 0) {
-              path.lineTo(scenePos, y);
-              path.lineTo(scenePos, yLow);
-            }
-            // todo add x and z
-            prev = dump.scals[idx].value;
-          }
-        } else {
           idx = this->isVector(dump, this->top->vars[i].identifier);
           // todo
           // vector drawing
+          isVector = true;
           if (!inited) {
-            if (dump.vecs[idx].type == 'b' || dump.vecs[idx].type == 'B') {
-              prev = dump.vecs[idx].valueVecDec;
-              inited = true;
-              path.moveTo(0, yHalf);
-            } else { 
+            prevFloat = dump.vecs[idx].valueVecDecFloat;
+            prev = dump.vecs[idx].valueVecDec;
+            inited = true;
+            path.moveTo(0, yHalf);
+            path.lineTo(HEXAGONS_STEP, y);
+            auxPath.moveTo(0, yHalf);
+            auxPath.lineTo(HEXAGONS_STEP, yLow);
 
+            float availLen = scenePos - prevScenePos;
+
+            prevScenePos = scenePos;
+
+          } else {
+            if ((prevFloat != dump.vecs[idx].valueVecDecFloat) ||
+                (prev != dump.vecs[idx].valueVecDec)) {
+              path.lineTo(scenePos - HEXAGONS_STEP, y);
+              path.lineTo(scenePos, yHalf);
+              path.lineTo(scenePos + HEXAGONS_STEP, y);
+
+              auxPath.lineTo(scenePos - HEXAGONS_STEP, yLow);
+              auxPath.lineTo(scenePos, yHalf);
+              auxPath.lineTo(scenePos + HEXAGONS_STEP, yLow);
+
+              float availLen = scenePos - prevScenePos;
+
+              // QTextItem
+              painter->save();
+              QFont font = painter->font();
+              font.setPixelSize(lineHeight - WAVES_GAP * 3);
+              painter->setFont(font);
+              QFontMetricsF fm(font);
+              std::string valInt = std::to_string(prev);
+              std::string valFloat = std::to_string(prevFloat);
+
+              QString val = QString::fromStdString(
+                  dump.vecs[idx].valueVecDec == 0 ? valFloat : valInt);
+              float valWidth = fm.horizontalAdvance(val);
+              QRectF rect = {prevScenePos, y, availLen,
+                              lineHeight - WAVES_GAP};
+              if (valWidth <= availLen) {
+                painter->drawText(rect, Qt::AlignCenter, val);
+              } else {
+                painter->drawText(rect, Qt::AlignCenter, ".");
+
+              }
+              painter->restore();
+
+              prevScenePos = scenePos;
+              prev = dump.vecs[idx].valueVecDec;
+              prevFloat = dump.vecs[idx].valueVecDecFloat;
+
+            } else {
+              // path.lineTo(scenePos);
+              path.lineTo(scenePos - HEXAGONS_STEP, y);
+              auxPath.lineTo(scenePos - HEXAGONS_STEP, yLow);
             }
+          }
+
+        } else {
+          idx = this->isScalar(dump, this->top->vars[i].identifier);
+          if (idx != -1) {
+
+            if (!inited) {
+              prev = dump.scals[idx].value;
+              inited = true;
+              // start low if the first bit is 0
+              if (prev == 0) {
+                path.moveTo(0, yLow);
+              }
+            } else {
+
+              // draw an angle depending on state
+              if (prev == 0 && dump.scals[idx].value == 1) {
+                path.lineTo(scenePos, yLow);
+                path.lineTo(scenePos, y);
+              }
+              if (prev == 1 && dump.scals[idx].value == 0) {
+                path.lineTo(scenePos, y);
+                path.lineTo(scenePos, yLow);
+              }
+              // todo add x and z
+              prev = dump.scals[idx].value;
+            }
+          } else {
+            // if (prev == 1) {
+            //   path.lineTo(scenePos, y);
+            // } else {
+            //   path.lineTo(scenePos, yLow);
+            // }
           }
         }
       };
       // qDebug() << "INFO" << j << a << b << width.width();
     }
     // when all the points are done, closing the line
-    if (prev == 1) {
+    if (!isVector) {
+
+      if (prev == 1) {
+        path.lineTo(width, y);
+      } else if (prev == 0) {
+        path.lineTo(width, yLow);
+      }
+    } else {
       path.lineTo(width, y);
-    } else if (prev == 0) {
-      path.lineTo(width, yLow);
+      auxPath.lineTo(width, yLow);
     }
     painter->drawPath(path);
+    painter->drawPath(auxPath);
     y += lineHeight;
   }
   this->top->setUpdatesEnabled(true);
@@ -116,13 +201,12 @@ int Waves::isScalar(DumpData data, std::string id) {
 
 int Waves::isVector(DumpData data, std::string id) {
   for (int i = 0; i < data.vecs.size(); i++) {
-    auto &d = data.scals[i];
+    auto &d = data.vecs[i];
     if (d.identifier == id) {
       return i;
     }
   }
   return -1;
 }
-
 
 QRectF Waves::boundingRect() const { return QRectF(0, 0, 10, 10); };
